620ebcb4efd05826c661dd8276f7b8036d2faf02
christianbennett <christianbennettrobin@gmail.com>
2021-06-08 19:24:35 +0700

    tes

--- SinSeiFS_B07.c
+++ SinSeiFS_B07.c
@@ -8,27 +8,362 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <sys/stat.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <math.h>
 #include <ctype.h>
 
-static const char *dirpath = "/home/bennett/Downloads";
-static const char *logpath = "/home/bennett/SinSeiFS.log";
+//path yang dibutuhkan
+static const char *dirPath = "/home/bennett/Downloads";
+static const char *logPath = "/home/bennett/SinSeiFS.log";
 
-int checkVersion(char path[]) //check versi no1 atau 2
+//array untuk menampung string yang diperlukan
+char *key = "SISOP";
+char *atoz = "AtoZ_";
+char *rx = "RX_";
+char *aisa = "A_is_a_";
+
+//mendapatkan lokasi "/""
+int slashId(char *path, int last)
 {
-    
+    for (int i = 0; i < strlen(path); i++)
+    {
+        if (path[i] == '/')
+            return i + 1;
+    }
+    return last;
 }
 
-void *getCurrentDir(char *path){ //untuk akses directory sekarang
-
+//mendapatkan lokasi "."
+int dotId(char *path)
+{
+    for (int i = strlen(path) - 1; i >= 0; i--)
+    {
+        if (path[i] == '.')
+            return i;
+    }
+    return strlen(path);
 }
 
-void enc(char *encrypt){ //encode
-
+//mendapatkan lokasi extension
+int extensionId(char *path)
+{
+    bool flag = 0;
+    for (int i = strlen(path) - 1; i >= 0; i--)
+    {
+        if (path[i] == '.')
+        {
+            if (flag == 1)
+                return i;
+            else
+                flag = 1;
+        }
+    }
+    return strlen(path);
 }
 
+void encryptAtbash(char *path)
+{
+    if (!strcmp(path, ".") || !strcmp(path, ".."))
+        return;
+
+    printf("Atbash Encrypt Path: %s\n", path);
+
+    int endid = split_ext_id(path);
+    if (endid == strlen(path))
+        endid = ext_id(path);
+    int startid = slash_id(path, 0);
+
+    for (int i = startid; i < endid; i++)
+    {
+        if (path[i] != '/' && isalpha(path[i]))
+        {
+            char tmp = path[i];
+            if (isupper(path[i]))
+                tmp -= 'A';
+            else
+                tmp -= 'a';
+            tmp = 25 - tmp; //Atbash cipher
+            if (isupper(path[i]))
+                tmp += 'A';
+            else
+                tmp += 'a';
+            path[i] = tmp;
+        }
+    }
+}
+
+void decryptAtbash(char *path)
+{
+    if (!strcmp(path, ".") || !strcmp(path, ".."))
+        return;
+
+    printf("Atbash Decrypt Path: %s\n", path);
+
+    int endid = split_ext_id(path);
+    if (endid == strlen(path))
+        endid = ext_id(path);
+    int startid = slash_id(path, endid);
+
+    for (int i = startid; i < endid; i++)
+    {
+        if (path[i] != '/' && isalpha(path[i]))
+        {
+            char tmp = path[i];
+            if (isupper(path[i]))
+                tmp -= 'A';
+            else
+                tmp -= 'a';
+            tmp = 25 - tmp; //Atbash cipher
+            if (isupper(path[i]))
+                tmp += 'A';
+            else
+                tmp += 'a';
+            path[i] = tmp;
+        }
+    }
+}
+
+void encryptRot13(char *path)
+{
+    if (!strcmp(path, ".") || !strcmp(path, ".."))
+        return;
+
+    printf("ROT14 Encrypt Path: %s\n", path);
+
+    int endid = split_ext_id(path);
+    int startid = slash_id(path, 0);
+
+    for (int i = startid; i < endid; i++)
+    {
+        if (path[i] != '/' && isalpha(path[i]))
+        {
+            char tmp = path[i];
+            if (isupper(path[i]))
+                tmp -= 'A';
+            else
+                tmp -= 'a';
+            tmp = (tmp + 13) % 26; //ROT13 cipher
+            if (isupper(path[i]))
+                tmp += 'A';
+            else
+                tmp += 'a';
+            path[i] = tmp;
+        }
+    }
+}
+
+void decryptRot13(char *path)
+{
+    if (!strcmp(path, ".") || !strcmp(path, ".."))
+        return;
+
+    printf("ROT13 Decrypt Path: %s\n", path);
+
+    int endid = split_ext_id(path);
+    int startid = slash_id(path, endid);
+
+    for (int i = startid; i < endid; i++)
+    {
+        if (path[i] != '/' && isalpha(path[i]))
+        {
+            char tmp = path[i];
+            if (isupper(path[i]))
+                tmp -= 'A';
+            else
+                tmp -= 'a';
+            tmp = (tmp + 13) % 26; //ROT13 cipher
+            if (isupper(path[i]))
+                tmp += 'A';
+            else
+                tmp += 'a';
+            path[i] = tmp;
+        }
+    }
+}
+
+void encryptVigenere(char *path)
+{
+    if (!strcmp(path, ".") || !strcmp(path, ".."))
+        return;
+
+    printf("Vigenere Encrypt Path: %s\n", path);
+
+    int endid = split_ext_id(path);
+    int startid = slash_id(path, 0);
+
+    for (int i = startid; i < endid; i++)
+    {
+        if (path[i] != '/' && isalpha(path[i]))
+        {
+            char tmp1 = path[i];
+            char tmp2 = key[(i - startid) % strlen(key)];
+            if (isupper(path[i]))
+            {
+                tmp1 -= 'A';
+                tmp2 -= 'A';
+                tmp1 = (tmp1 + tmp2) % 26; //Vigenere cipher
+                tmp1 += 'A';
+            }
+            else
+            {
+                tmp1 -= 'a';
+                tmp2 = tolower(tmp2) - 'a';
+                tmp1 = (tmp1 + tmp2) % 26; //Vigenere cipher
+                tmp1 += 'a';
+            }
+            path[i] = tmp1;
+        }
+    }
+}
+
+void decryptVigenere(char *path)
+{
+    if (!strcmp(path, ".") || !strcmp(path, ".."))
+        return;
+
+    printf("Vigenere Decrypt Path: %s\n", path);
+
+    int endid = split_ext_id(path);
+    int startid = slash_id(path, endid);
+
+    for (int i = startid; i < endid; i++)
+    {
+        if (path[i] != '/' && isalpha(path[i]))
+        {
+            char tmp1 = path[i];
+            char tmp2 = key[(i - startid) % strlen(key)];
+            if (isupper(path[i]))
+            {
+                tmp1 -= 'A';
+                tmp2 -= 'A';
+                tmp1 = (tmp1 - tmp2 + 26) % 26; //Vigenere cipher
+                tmp1 += 'A';
+            }
+            else
+            {
+                tmp1 -= 'a';
+                tmp2 = tolower(tmp2) - 'a';
+                tmp1 = (tmp1 - tmp2 + 26) % 26; //Vigenere cipher
+                tmp1 += 'a';
+            }
+            path[i] = tmp1;
+        }
+    }
+}
+
+void encrypt2(char *fpath)
+{
+    chdir(fpath);
+    DIR *dp;
+    struct dirent *dir;
+
+    dp = opendir(".");
+    if (dp == NULL)
+        return;
+    struct stat st;
+    char dirPath[1000];
+    char filePath[1000];
+
+    while ((dir = readdir(dp)) != NULL)
+    {
+        printf("dirname %s\n", dir->d_name);
+        printf("%s/%s\n", fpath, dir->d_name);
+        if (stat(dir->d_name, &st) < 0)
+            ;
+        else if (S_ISDIR(st.st_mode))
+        {
+            if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)
+                continue;
+            sprintf(dirPath, "%s/%s", fpath, dir->d_name);
+            encrypt2(dirPath);
+            printf("dirPath %s\n", dirPath);
+        }
+        else
+        {
+            sprintf(filePath, "%s/%s", fpath, dir->d_name);
+            FILE *input = fopen(filePath, "r");
+            if (input == NULL)
+                return;
+            int bytes_read, count = 0;
+            void *buffer = malloc(1024);
+            while ((bytes_read = fread(buffer, 1, 1024, input)) > 0)
+            {
+                char newFilePath[1000];
+                sprintf(newFilePath, "%s.%04d", filePath, count);
+                count++;
+                FILE *output = fopen(newFilePath, "w+");
+                if (output == NULL)
+                    return;
+                fwrite(buffer, 1, bytes_read, output);
+                fclose(output);
+                memset(buffer, 0, 1024);
+            }
+            fclose(input);
+            printf("filepath %s\n", filePath);
+            remove(filePath);
+        }
+    }
+    closedir(dp);
+}
+
+void decrypt2(char *dir)
+{
+    chdir(dir);
+    DIR *dp;
+    struct dirent *de;
+    struct stat st;
+    dp = opendir(".");
+    if (dp == NULL)
+        return;
+
+    char dirPath[1000];
+    char filePath[1000];
+
+    while ((de = readdir(dp)) != NULL)
+    {
+        if (stat(de->d_name, &st) < 0)
+            ;
+        else if (S_ISDIR(st.st_mode))
+        {
+            if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0)
+                continue;
+            sprintf(dirPath, "%s/%s", dir, de->d_name);
+            decrypt2(dirPath);
+        }
+        else
+        {
+            sprintf(filePath, "%s/%s", dir, de->d_name);
+            filePath[strlen(filePath) - 5] = '\0';
+            FILE *check = fopen(filePath, "r");
+            if (check != NULL)
+                return;
+            FILE *file = fopen(filePath, "w");
+            int count = 0;
+            char topath[1000];
+            sprintf(topath, "%s.%04d", filePath, count);
+            void *buffer = malloc(1024);
+            while (1)
+            {
+                FILE *op = fopen(topath, "rb");
+                if (op == NULL)
+                    break;
+                size_t readSize = fread(buffer, 1, 1024, op);
+                fwrite(buffer, 1, readSize, file);
+                fclose(op);
+                remove(topath);
+                count++;
+                sprintf(topath, "%s.%04d", filePath, count);
+            }
+            free(buffer);
+            fclose(file);
+        }
+    }
+    closedir(dp);
+}
+
+//untuk mendapatkan strlen tanpa extension
 int len(char str[])
 {
     int count = 0;
@@ -44,6 +379,7 @@ int len(char str[])
     return count;
 }
 
+//compare 2 string jika beda
 void compareString(char str1[], char str2[], char arr[])
 {
     for (int i = 0; i < len(str1); i++)
@@ -59,6 +395,7 @@ void compareString(char str1[], char str2[], char arr[])
     }
 }
 
+//convert decimal to binary
 int convert(long long int n)
 {
     int dec = 0, i = 0, rem;
@@ -72,6 +409,7 @@ int convert(long long int n)
     return dec;
 }
 
+//concat pada string
 void appendString(char str1[])
 {
     char str2[strlen(str1)];
@@ -103,15 +441,112 @@ void appendString(char str1[])
     // printf("str1 = %s\n", str1);
     // printf("decimalString = %s\n", decimalString);
 
+    for (int i = 0; str1[i]; i++)
+    {
+        str1[i] = tolower(str1[i]);
+    }
+
     strcat(str1, ".");
     strcat(str1, decimalString);
 
     // printf("New String = %s", str1);
 }
 
+void encryptBinary(char *fpath)
+{
+    chdir(fpath);
+
+    DIR *dp;
+    struct dirent *dir;
+    struct stat st;
+
+    dp = opendir(".");
+    if (dp == NULL)
+        return;
+
+    char dirPath[1000];
+    char filePath[1000];
+    char binaryFilePath[1000];
+    char temp[1000];
+
+    while ((dir = readdir(dp)) != NULL)
+    {
+        if (stat(dir->d_name, &st) < 0)
+            ;
+        else if (S_ISDIR(st.st_mode))
+        {
+            if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)
+                continue;
+            sprintf(dirPath, "%s/%s", fpath, dir->d_name);
+            encryptBinary(dirPath);
+        }
+        else
+        {
+            sprintf(filePath, "%s/%s", fpath, dir->d_name);
+            sprintf(temp, "%s", dir->d_name);
+            appendString(temp);
+            sprintf(binaryFilePath, "%s/%s", fpath, temp);
+            rename(filePath, binaryFilePath);
+        }
+    }
+    closedir(dp);
+}
+
+void decryptBinary(char *fpath)
+{
+    // chdir(fpath);
+
+    // DIR *dp;
+    // struct dirent *dir;
+    // struct stat st;
+
+    // dp = opendir(".");
+    // if (dp == NULL)
+    //     return;
+
+    // char dirPath[1000];
+    // char filePath[1000];
+    // char decimalFilePath[1000];
+    // char fname[1000];
+    // char bin[1000];
+    // char normalcase[1000];
+    // char clearPath[1000];
+
+    // while ((dir = readdir(dp)) != NULL)
+    // {
+    //     if (stat(dir->d_name, &st) < 0)
+    //         ;
+    //     else if (S_ISDIR(st.st_mode))
+    //     {
+    //         if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0)
+    //             continue;
+    //         sprintf(dirPath, "%s/%s", fpath, dir->d_name);
+    //         decryptBinary(dirPath);
+    //     }
+    //     else
+    //     {
+    //         sprintf(filePath, "%s/%s", fpath, dir->d_name);
+
+    //         strcpy(fname, dir->d_name);
+    //         char *ext = strrchr(fname, '.');
+    //         int dec = convertDec(ext + 1);
+    //         for (int i = 0; i < strlen(fname) - strlen(ext); i++)
+    //             clearPath[i] = fname[i];
+
+    //         char *ext2 = strrchr(clearPath, '.');
+    //         dec_to_bin(dec, bin, strlen(clearPath) - strlen(ext2));
+    //         getDecimal(clearPath, bin, normalcase);
+    //         sprintf(decimalFilePath, "%s/%s", fpath, normalcase);
+    //         rename(filePath, decimalFilePath);
+    //     }
+    // }
+    // closedir(dp);
+}
+
+//fungsi log no.4
 void logFile(char *level, char *command, const char *desc[], int descLen)
 {
-    FILE *f = fopen(logpath, "a");
+    FILE *f = fopen(logPath, "a");
     time_t t;
     struct tm *tmp;
     char timeString[100];
@@ -135,7 +570,28 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
     int res;
     char fpath[1000];
 
-    sprintf(fpath, "%s%s", dirpath, path);
+    // sprintf(fpath, "%s%s", dirPath, path);
+
+    if (strstr(path, atoz) != NULL)
+    {
+        decryptAtbash(strstr(path, atoz));
+    }
+
+    if (strstr(path, rx) != NULL)
+    {
+        decryptRot13(strstr(path, rx));
+        decryptAtbash(strstr(path, rx));
+    }
+
+    if (strcmp(path, "/") == 0)
+    {
+        path = dirPath;
+        sprintf(fpath, "%s", path);
+    }
+    else
+    {
+        sprintf(fpath, "%s%s", dirPath, path);
+    }
 
     res = lstat(fpath, stbuf);
 
@@ -152,13 +608,24 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
 {
     char fpath[1000];
 
+    if (strstr(path, atoz) != NULL)
+    {
+        decryptAtbash(strstr(path, atoz));
+    }
+
+    if (strstr(path, rx) != NULL)
+    {
+        decryptRot13(strstr(path, rx));
+        decryptAtbash(strstr(path, rx));
+    }
+
     if (strcmp(path, "/") == 0)
     {
-        path = dirpath;
+        path = dirPath;
         sprintf(fpath, "%s", path);
     }
     else
-        sprintf(fpath, "%s%s", dirpath, path);
+        sprintf(fpath, "%s%s", dirPath, path);
 
     int res = 0;
 
@@ -174,14 +641,22 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
 
     while ((de = readdir(dp)) != NULL)
     {
+        if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0)
+            continue;
+
         struct stat st;
-
         memset(&st, 0, sizeof(st));
-
         st.st_ino = de->d_ino;
         st.st_mode = de->d_type << 12;
-        res = (filler(buf, de->d_name, &st, 0));
 
+        if (a != NULL)
+            encryptAtbash(de->d_name);
+        if (b != NULL)
+        {
+            encryptAtbash(de->d_name);
+            encryptRot13(de->d_name);
+        }
+        res = (filler(buf, de->d_name, &st, 0));
         if (res != 0)
             break;
     }
@@ -197,14 +672,26 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
     char fpath[1000];
+
+    if (strstr(path, atoz) != NULL)
+    {
+        decryptAtbash(strstr(path, atoz));
+    }
+
+    if (strstr(path, rx) != NULL)
+    {
+        decryptRot13(strstr(path, rx));
+        decryptAtbash(strstr(path, rx));
+    }
+
     if (strcmp(path, "/") == 0)
     {
-        path = dirpath;
+        path = dirPath;
 
         sprintf(fpath, "%s", path);
     }
     else
-        sprintf(fpath, "%s%s", dirpath, path);
+        sprintf(fpath, "%s%s", dirPath, path);
 
     int res = 0;
     int fd = 0;
@@ -223,7 +710,7 @@ static int xmp_read(const char *path, char *buf, size_t size, off_t offset, stru
 
     close(fd);
 
-    const char *desc[] = {path};
+    const char *desc[] = {fpath};
     logFile("INFO", "READ", desc, 1);
 
     return res;
@@ -286,11 +773,34 @@ static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
 static int xmp_mkdir(const char *path, mode_t mode)
 {
     int res;
+    char fpath[1000];
 
-    res = mkdir(path, mode);
+    if (strstr(path, atoz) != NULL)
+    {
+        decryptAtbash(strstr(path, atoz));
+    }
+
+    if (strstr(path, rx) != NULL)
+    {
+        decryptRot13(strstr(path, rx));
+        decryptAtbash(strstr(path, rx));
+    }
+
+    if (strcmp(path, "/") == 0)
+    {
+        path = dirPath;
+        sprintf(fpath, "%s", path);
+    }
+
+    else
+    {
+        sprintf(fpath, "%s%s", dirPath, path);
+    }
+
+    res = mkdir(fpath, mode);
     if (res == -1)
         return -errno;
-    const char *desc[] = {path};
+    const char *desc[] = {fpath};
     logFile("INFO", "MKDIR", desc, 1);
 
     return 0;
@@ -312,12 +822,35 @@ static int xmp_unlink(const char *path)
 static int xmp_rmdir(const char *path)
 {
     int res;
+    char fpath[1000];
 
-    res = rmdir(path);
+    if (strstr(path, atoz) != NULL)
+    {
+        decryptAtbash(strstr(path, atoz));
+    }
+
+    if (strstr(path, rx) != NULL)
+    {
+        decryptRot13(strstr(path, rx));
+        decryptAtbash(strstr(path, rx));
+    }
+
+    if (strcmp(path, "/") == 0)
+    {
+        path = dirPath;
+        sprintf(fpath, "%s", path);
+    }
+
+    else
+    {
+        sprintf(fpath, "%s%s", dirPath, path);
+    }
+
+    res = rmdir(fpath);
     if (res == -1)
         return -errno;
 
-    const char *desc[] = {path};
+    const char *desc[] = {fpath};
     logFile("WARNING", "RMDIR", desc, 1);
 
     return 0;
@@ -340,15 +873,55 @@ static int xmp_symlink(const char *from, const char *to)
 static int xmp_rename(const char *from, const char *to)
 {
     int res;
+    char frompath[1000], topath[1000];
 
-    res = rename(from, to);
+    char *a = strstr(to, atoz);
+    if (a != NULL)
+        decryptAtbash(a);
+
+    char *b = strstr(from, rx);
+    if (b != NULL)
+    {
+        decryptRot13(b);
+        decryptAtbash(b);
+    }
+
+    char *c = strstr(to, rx);
+    if (c != NULL)
+    {
+        decryptRot13(c);
+        decryptAtbash(c);
+    }
+
+    sprintf(frompath, "%s%s", dirPath, from);
+    sprintf(topath, "%s%s", dirPath, to);
+
+    res = rename(frompath, topath);
     if (res == -1)
         return -errno;
 
-    const char *desc[] = {from, to};
+    const char *desc[] = {frompath, topath};
     logFile("INFO", "RENAME", desc, 2);
 
-    return 0;
+    if (c != NULL)
+    {
+        enkripsi2(topath);
+    }
+
+    if (b != NULL && c == NULL)
+    {
+        dekripsi2(topath);
+    }
+
+    if (strstr(to, aisa) != NULL)
+    {
+        encryptBinary(topath);
+    }
+
+    if (strstr(from, aisa) != NULL && strstr(to, aisa) == NULL)
+    {
+        decryptBinary(topath);
+    }
 }
 
 static int xmp_link(const char *from, const char *to)
